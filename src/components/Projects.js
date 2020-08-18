cb7fc42db3e651f7516ee8a2bdc04a4d94effebb
Jeremy Grancher <jgrancher@google.com>
2020-08-19 01:07:31 +1000

    Removed unused code & image

--- src/components/Projects.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Image, Text, View } from "@react-pdf/renderer";
-import theme from "../theme";
-
-const Projects = () => {
-  return <View></View>;
-};
-
-export default Projects;
